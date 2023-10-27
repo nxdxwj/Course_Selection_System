a578610d6ca2a7505558ee0cbc286b65946932ea
nxdxwj <202383910029@nuist.edu.cn>
2023-10-27 20:17:20 +0800

    Update

--- src/com/nuist/main/Entrance.java
+++ src/com/nuist/main/Entrance.java
@@ -2,6 +2,6 @@ package com.nuist.main;
 
 public class Entrance {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/com/nuist/menu/Run.java
@@ -0,0 +1,4 @@
+package com.nuist.menu;
+
+public class Run {
+}
