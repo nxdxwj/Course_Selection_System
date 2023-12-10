0c3c06b0513a2b0960a5246abd116bfb2fdc2831
Sage542 <146344223+Sage542@users.noreply.github.com>
2023-12-10 12:18:30 +0800

    Update Entrance.java

--- src/java/com/nuist/main/Entrance.java
+++ src/java/com/nuist/main/Entrance.java
@@ -1,11 +1,1 @@
-package com.nuist.main;
 
-import com.nuist.menu.Run;
-
-public class Entrance {
-    public static void main(String[] args) {
-        //The main entrance of the whole program
-        Run run = new Run();
-        run.runMenu();
-    }
-}
