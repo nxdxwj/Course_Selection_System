c25ad67d80e4d9257bcdbaca3b2e70167bbaced5
Sage542 <146344223+Sage542@users.noreply.github.com>
2023-12-11 15:43:54 +0800

    Update Display.java

--- src/java/com/nuist/menu/Display.java
+++ src/java/com/nuist/menu/Display.java
@@ -1,1 +1,26 @@
 
+package com.nuist.menu;
+
+import java.util.Scanner;
+
+public class Display {
+    public int dispalyMenu(){
+        System.out.println("""
+                Course Selection System
+                Welcome~~~
+                ------------------
+                1) Log in the NUIST Course Selection System
+                2) Creat a new account
+                0)Exit
+                (Please enter your option)
+                ======>>             
+                """
+        );
+        Scanner input = new Scanner(System.in);
+        return  input.nextInt();
+
+
+
+    }
+}
+
