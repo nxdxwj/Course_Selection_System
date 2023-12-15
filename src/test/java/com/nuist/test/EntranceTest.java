7801e6632797dc46879f5e71d7e2ba96ea6aa9e2
nxdyaoxuan <202383910031@nuist.edu.cn>
2023-12-15 20:27:58 +0800

    automatic test

---
+++ src/test/java/com/nuist/test/EntranceTest.java
@@ -0,0 +1,14 @@
+package com.nuist.test;
+
+import com.nuist.main.Entrance;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntranceTest {
+
+    @Test
+    void testMain() {
+
+    }
+}
