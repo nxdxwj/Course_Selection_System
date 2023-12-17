18d5a57f5ad643b1a0306b38814e0bbc402041cc
nxdyaoxuan <202383910031@nuist.edu.cn>
2023-12-17 16:11:26 +0800

    update

--- src/java/com/nuist/menu/Display.java
+++ src/java/com/nuist/menu/Display.java
@@ -6,7 +6,14 @@ import java.util.Scanner;
 public class Display {
     public int dispalyMenu(){
         System.out.println("""
-                     
+                Course Selection System
+                Welcome~~~
+                ------------------
+                1) Log in the NUIST Course Selection System
+                2) Creat a new account
+                0)Exit
+                (Please enter your option)
+                ======>>             
                 """
         );
         Scanner input = new Scanner(System.in);
